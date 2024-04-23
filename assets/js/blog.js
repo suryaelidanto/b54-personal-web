070c3081ebeb5bd0367c90370e66986562d0adeb
suryaelidanto <suryaelidanto@gmail.com>
2024-04-23 11:16:43 +0700

    feat: routing

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,158 +1,158 @@
-const blogs = [];
+// const blogs = [];
 
-function addBlog(e) {
-  e.preventDefault();
+// function addBlog(e) {
+//   e.preventDefault();
 
-  const title = document.getElementById("input-blog-title").value; // title 1
-  const content = document.getElementById("input-blog-content").value; // content 1
-  let image = document.getElementById("input-blog-image").files; // cat.jpg
-  // const nodejs = document.getElementById("nodejs").checked // checked = true
-  // const python = document.getElementById("python").checked
+//   const title = document.getElementById("input-blog-title").value; // title 1
+//   const content = document.getElementById("input-blog-content").value; // content 1
+//   let image = document.getElementById("input-blog-image").files; // cat.jpg
+//   // const nodejs = document.getElementById("nodejs").checked // checked = true
+//   // const python = document.getElementById("python").checked
 
-  // let tech = []
+//   // let tech = []
 
-  // for (let x = 0; x < technologies.length; x++) {
-  //     if (technologies[x].checked) {
-  //         tech.push(technologies[x].value)
-  //     }
-  // }
+//   // for (let x = 0; x < technologies.length; x++) {
+//   //     if (technologies[x].checked) {
+//   //         tech.push(technologies[x].value)
+//   //     }
+//   // }
 
-  image = URL.createObjectURL(image[0]);
+//   image = URL.createObjectURL(image[0]);
 
-  const createdAt = new Date();
+//   const createdAt = new Date();
 
-  // localStorage.setItem("blog", JSON.stringify({
-  //     title,
-  //     content,
-  //     imageLink
-  // }))
+//   // localStorage.setItem("blog", JSON.stringify({
+//   //     title,
+//   //     content,
+//   //     imageLink
+//   // }))
 
-  const blog = {
-    title,
-    content,
-    image,
-    createdAt,
-  };
+//   const blog = {
+//     title,
+//     content,
+//     image,
+//     createdAt,
+//   };
 
-  console.log("blog");
-  blogs.unshift(blog);
-  renderBlog();
+//   console.log("blog");
+//   blogs.unshift(blog);
+//   renderBlog();
 
-  console.log("blogs", blogs);
-}
+//   console.log("blogs", blogs);
+// }
 
-function renderBlog() {
-  let html = "";
+// function renderBlog() {
+//   let html = "";
 
-  for (let index = 0; index < blogs.length; index++) {
-    // let renderTechIcons = ''
+//   for (let index = 0; index < blogs.length; index++) {
+//     // let renderTechIcons = ''
 
-    // if (blogs[index].nodejs) {
-    //     renderTechIcons += `<i class="fa-brands fa-node"></i>`
-    // }
+//     // if (blogs[index].nodejs) {
+//     //     renderTechIcons += `<i class="fa-brands fa-node"></i>`
+//     // }
 
-    // if (blogs[index].python) {
-    //     renderTechIcons += `<i class="fa-brands fa-python"></i>`
-    // }
+//     // if (blogs[index].python) {
+//     //     renderTechIcons += `<i class="fa-brands fa-python"></i>`
+//     // }
 
-    html += `
-        <div class="card d-flex flex-row">
-        <img
-          src="${blogs[index].image}"
-          class="card-img-top w-25"
-          alt="..."
-        />
-        <div class="card-body d-flex flex-column align-items-start">
-          <div class="w-100 d-flex justify-content-end">
-            <a href="#" class="btn btn-secondary mx-2">Edit Post</a>
-            <a href="#" class="btn btn-warning">Delete Post</a>
-          </div>
-          <h5 class="card-title fw-bold">${blogs[index].title}</h5>
-            <p class="card-text">
-            ${getFullTime(blogs[index].createdAt)} | Ichsan Emrald Alamsyah
-            </p>
-          <p class="card-text">
-          ${blogs[index].content}
-          </p>
-          <p class="card-text">
-          ${getDistanceTime(blogs[index].createdAt)}
-          </p>
-        </div>
-      </div>
-`;
-  }
+//     html += `
+//         <div class="card d-flex flex-row">
+//         <img
+//           src="${blogs[index].image}"
+//           class="card-img-top w-25"
+//           alt="..."
+//         />
+//         <div class="card-body d-flex flex-column align-items-start">
+//           <div class="w-100 d-flex justify-content-end">
+//             <a href="#" class="btn btn-secondary mx-2">Edit Post</a>
+//             <a href="#" class="btn btn-warning">Delete Post</a>
+//           </div>
+//           <h5 class="card-title fw-bold">${blogs[index].title}</h5>
+//             <p class="card-text">
+//             ${getFullTime(blogs[index].createdAt)} | Ichsan Emrald Alamsyah
+//             </p>
+//           <p class="card-text">
+//           ${blogs[index].content}
+//           </p>
+//           <p class="card-text">
+//           ${getDistanceTime(blogs[index].createdAt)}
+//           </p>
+//         </div>
+//       </div>
+// `;
+//   }
 
-  document.getElementById("contents").innerHTML = html;
-}
+//   document.getElementById("contents").innerHTML = html;
+// }
 
-function getFullTime(dates) {
-  // date formatter
-  let minutes = dates.getMinutes();
-  let hours = dates.getHours();
-  const date = dates.getDate();
-  const month = dates.getMonth();
-  const year = dates.getFullYear();
+// function getFullTime(dates) {
+//   // date formatter
+//   let minutes = dates.getMinutes();
+//   let hours = dates.getHours();
+//   const date = dates.getDate();
+//   const month = dates.getMonth();
+//   const year = dates.getFullYear();
 
-  // solusi 1 : bikin condition
+//   // solusi 1 : bikin condition
 
-  // if(bulan == 0) bulan = "January"
-  // if(bulan == 1) bulan = "February"
-  // if(bulan == 2) bulan = "March"
+//   // if(bulan == 0) bulan = "January"
+//   // if(bulan == 1) bulan = "February"
+//   // if(bulan == 2) bulan = "March"
 
-  // solusi 2 : bikin array, isinya January - December
-  if (hours < 10) {
-    hours = "0" + hours;
-  }
+//   // solusi 2 : bikin array, isinya January - December
+//   if (hours < 10) {
+//     hours = "0" + hours;
+//   }
 
-  if (minutes < 10) {
-    minutes = "0" + minutes;
-  }
+//   if (minutes < 10) {
+//     minutes = "0" + minutes;
+//   }
 
-  const months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
+//   const months = [
+//     "January",
+//     "February",
+//     "March",
+//     "April",
+//     "May",
+//     "June",
+//     "July",
+//     "August",
+//     "September",
+//     "October",
+//     "November",
+//     "December",
+//   ];
 
-  return `${date} ${months[month]} ${year} ${hours}:${minutes} WIB`;
-}
+//   return `${date} ${months[month]} ${year} ${hours}:${minutes} WIB`;
+// }
 
-function getDistanceTime(timePost) {
-  // fungsi untuk menghitung jarak antara waktu ketika kita posting blog dengan waktu sekarang
-  let timeNow = new Date();
+// function getDistanceTime(timePost) {
+//   // fungsi untuk menghitung jarak antara waktu ketika kita posting blog dengan waktu sekarang
+//   let timeNow = new Date();
 
-  let distance = timeNow - timePost; // hasilnya miliseconds -> 1 detik = 1000ms
+//   let distance = timeNow - timePost; // hasilnya miliseconds -> 1 detik = 1000ms
 
-  // Math.floor() // membulatkan ke bawah, ex : 1.3 -> 1
-  // Math.round() // membulatkan ke terdekat, ex : 1.3 -> 1 OR 1.7 -> 2
-  // Math.ceil() // membulatkan ke atas, ex : 1.3 -> 2
-  const seconds = Math.floor(distance / 1000);
-  const minutes = Math.floor(distance / 1000 / 60);
-  const hours = Math.floor(distance / 1000 / 60 / 60);
-  const day = Math.floor(distance / 1000 / 60 / 60 / 24);
+//   // Math.floor() // membulatkan ke bawah, ex : 1.3 -> 1
+//   // Math.round() // membulatkan ke terdekat, ex : 1.3 -> 1 OR 1.7 -> 2
+//   // Math.ceil() // membulatkan ke atas, ex : 1.3 -> 2
+//   const seconds = Math.floor(distance / 1000);
+//   const minutes = Math.floor(distance / 1000 / 60);
+//   const hours = Math.floor(distance / 1000 / 60 / 60);
+//   const day = Math.floor(distance / 1000 / 60 / 60 / 24);
 
-  if (seconds < 60) {
-    return seconds + " seconds ago";
-  } else if (minutes < 60) {
-    return minutes + " minutes ago";
-  } else if (hours < 60) {
-    return hours + " hours ago";
-  } else if (day < 24) {
-    return day + " day ago";
-  }
-}
+//   if (seconds < 60) {
+//     return seconds + " seconds ago";
+//   } else if (minutes < 60) {
+//     return minutes + " minutes ago";
+//   } else if (hours < 60) {
+//     return hours + " hours ago";
+//   } else if (day < 24) {
+//     return day + " day ago";
+//   }
+// }
 
-renderBlog();
+// renderBlog();
 
-setInterval(() => {
-  renderBlog();
-}, 1000);
+// setInterval(() => {
+//   renderBlog();
+// }, 1000);
--- index.js
+++ index.js
@@ -1,12 +1,70 @@
-const express = require("express")
-const app = express()
-const port = 5000
+const express = require("express");
+const app = express();
+const port = 5000;
+const path = require("path");
+
+// app.set = setting variable global, configuration, dll
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "./views"));
+
+// app.use = setting middleware
+app.use("/assets", express.static(path.join(__dirname, "./assets")));
+
+// body parser
+app.use(express.urlencoded({ extended: false }));
+// extended : false => querystring bawaan dari express
+// extended : true = > menggunakan query strign third party => qs
 
 // route
-app.get("/hello", (req, res) => {
-    res.send("Hello world!")
-})
+app.get("/", home);
+app.get("/blog", blog);
+app.post("/blog", addBlog);
+app.get("/contact", contact);
+app.get("/testimonial", testimonial);
+app.get("/blog-detail/:id", blogDetail);
+
+const data = [];
+
+// service
+function home(req, res) {
+  res.render("index");
+}
+
+function blog(req, res) {
+  res.render("blog", { data });
+}
+
+function addBlog(req, res) {
+  const { title, content } = req.body;
+
+  data.push({
+    title,
+    content,
+  });
+
+  res.redirect("blog");
+}
+
+function contact(req, res) {
+  res.render("contact");
+}
+
+function testimonial(req, res) {
+  res.render("testimonial");
+}
+
+function blogDetail(req, res) {
+  const { id } = req.params;
+
+    const data = {
+        id: id,
+        title: "Title 1",
+        content : "Content 1"
+    }
+
+  res.render("blog-detail", {data: data});
+}
 
 app.listen(port, () => {
-    console.log("Server is running on PORT :", port)
-})
+  console.log("Server is running on PORT :", port);
+});
